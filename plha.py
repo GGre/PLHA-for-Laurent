a63df618e60a8d82766ce9cc9ce69588fe506ba9
Georges Gressot <gressot@unistra.fr>
2018-01-26 14:29:03 +0100

    Update plha.py

--- plha.py
+++ plha.py
@@ -1,7 +1,7 @@
 # This script creates in a single directory a suite of ##### subdirectories of the following form :
 # {pre}{#####} where {pre} is an invariable prefix and {#####} is a five digits number progressing
 # by unity from 00001 to (here) 25000.
-# It shall be ran with the command >>>python3 plha.py
+# It shall be ran with the command : >>>python3 plha.py
 
 pre, i, j, k = PLHA, 25001, 0, 25000                                # Prefix, increment, except count, nbr of waited directories
 
@@ -26,4 +26,4 @@ while i:
         j += 1
         continue
 
-print(k-j, ' directories created (', j, ' exception(s) occured)')
+print(k-j, ' directories created (', j, ' exception(s) occured)')   # Edition of the final report
