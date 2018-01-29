bec53a9d590b337a9f2fd3b6ee08658e6e62fc02
Georges Gressot <gressot@unistra.fr>
2018-01-29 18:40:29 +0100

    Delete moving.py

--- moving.py
+++
@@ -1,9 +0,0 @@
-# This script for moving files PLHA#####a and PLHA#####b to the right folder PLHA#####
-
-inf = input("Give the lower number of the numbered images")
-sup = input("Give the upper number of the numebred images")
-
-i = int(sup)
-
-while i:
-    move 
