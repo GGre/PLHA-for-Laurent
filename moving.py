2f8e86a79ed17fae9a379e84dd463680aa580518
Georges Gressot <gressot@unistra.fr>
2018-01-29 18:36:29 +0100

    Create moving.py

---
+++ moving.py
@@ -0,0 +1,8 @@
+# This script for moving files PLHA#####a and PLHA#####b to the right folder PLHA#####
+
+inf = input("Give the lower number of the numbered images")
+sup = input("Give the upper number of the numebred images")
+
+i = sup
+
+while i:
