3e977e2e2fa63121d6340b63e326bb55a3b8c6e1
Georges Gressot <gressot@unistra.fr>
2018-01-29 15:00:49 +0100

    Update plha.py

--- plha.py
+++ plha.py
@@ -1,29 +1,26 @@
-# This script creates in a single directory a a same level suite suite of ##### subdirectories of the following form :
-# pre##### where pre is an invariable prefix and ##### is a five digits number progressing
-# by unity from 00001 to (here) 25000.
+# This script creates in a single directory a same level suite of ##### subdirectories of the following form :
+# pre##### where pre is an invariable prefix and ##### is a five digits number progressing by unity from 00001
+# to (here) 25000.
 # Run it with the command : >>>python3 plha.py
 
-pre, i, j, k = PLHA, 25001, 0, 25000                                # Prefix, max-increment, except count, nbr of waited directories
+i, j = 25000, 25000
+pre = "/home/ladmin/Téléchargements/PLHA"
 
-from os import chdir, mkdir
-
-chdir("/home/bidon/bidon")                                          # Change with the wishing path
+import os
 
 while i:
-    i -= 1
-    try:
-        if i < 10000:
-            name = pre + '0' + str(i)
-        elif i < 1000:
-            name = pre + '00' + str(i)
-        elif i < 100:
-            name = pre + '000' + str(i)
-        elif i < 10:
-            name = pre + '0000' + str(i)
-        mkdir name
-    except Exception as err:
-        print(err)
-        j += 1
-        continue
+    if i > 9999:
+        suf = str(i)
+    elif i > 999 and i < 10000:
+        suf = '0' + str(i)
+    elif i > 99 and i < 1000:
+        suf = '00' + str(i)
+    elif i > 9 and i < 100:
+        suf = '000' + str(i)
+    elif i < 10:
+        suf = '0000' + str(i)
+    path = pre + suf
+    os.mkdir(path)
+    i -=1
 
-print(k-j, ' directories created (', j, ' exception(s) occured)')   # Final report edition
+print(j, ' directories created')
