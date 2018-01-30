959d7ee6590251963e927d81c8819835e47527f2
Georges Gressot <gressot@unistra.fr>
2018-01-30 08:58:05 +0100

    Update plha.py

--- plha.py
+++ plha.py
@@ -1,10 +1,14 @@
-# This script creates in a single directory a same level suite of ##### subdirectories of the following form :
-# pre##### where pre is an invariable prefix and ##### is a five digits number progressing by unity from 00001
-# to (here) 25000.
+# This script creates in a single directory (here : /home/ladmin/Téléchargements/) a same level suite of ##### subdirectories
+# of the following form : pref##### where pref is an invariable prefix and ##### is a five digits number progressing by unity
+# from 00001 to (here) 25000.
 # Run it with the command : >>>python3 plha.py
 
 i, j = 25000, 25000
-#pre = "/home/ladmin/Téléchargements/PLHA"    # Change the path and cancel the quote 
+#way = "/home/ladmin/Téléchargements/PLHA"    # Change the path to the wished one (see below N.B. 1 & 2)
+                                              # Cancel the quote
+                                              # N.B. 1: PLHA will be the prefix pref
+                                              # N.B. 2: Directories PLHA##### will be created here in
+                                              # /home/ladmin/Téléchargements/
 
 import os
 
@@ -19,7 +23,7 @@ while i:
         suf = '000' + str(i)
     elif i < 10:
         suf = '0000' + str(i)
-    path = pre + suf
+    path = way + suf
     os.mkdir(path)
     i -=1
 
